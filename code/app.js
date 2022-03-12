c22cfd21c2741a7ee8f9ce9b37d3b5e5437a6f23
flet-pro <63170fu@seiko.ac.jp>
2022-03-12 12:41:44 +0900

    Create index.html

---
+++ code/app.js
@@ -0,0 +1,25 @@
+let circle = document.getElementById("circle");
+let upBt = document.getElementById("upBt");
+let downBt = document.getElementById("downBt");
+//console.log(circle);
+let rorateValue = circle.style.transform;
+let rotateSum;
+
+console.log(rorateValue);
+
+upBt.onclick = function(){
+    /*反時計回り*/
+    rotateSum = rorateValue + "rotate(-90deg)";
+    circle.style.transform = rotateSum;
+    rorateValue = rotateSum;
+    //let rorateValue = circle.style.transform;
+    // is this hard for CPU?
+} 
+downBt.onclick = function(){
+    /*時計回り*/
+    rotateSum = rorateValue + "rotate(90deg)";
+    circle.style.transform = rotateSum;
+    rorateValue = rotateSum;
+    //let rorateValue = circle.style.transform;
+    // is this hard for CPU?
+} 
